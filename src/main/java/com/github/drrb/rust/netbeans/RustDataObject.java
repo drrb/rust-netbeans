8d6e7bc8e5dee80308d17658242b995914dced3d
drrb <drrrrrrrrrrrb@gmail.com>
2013-07-08 22:16:42 +1000

    Added Rust file type

---
+++ src/main/java/com/github/drrb/rust/netbeans/RustDataObject.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.drrb.rust.netbeans;
+
+import java.io.IOException;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.MIMEResolver;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectExistsException;
+import org.openide.loaders.MultiDataObject;
+import org.openide.loaders.MultiFileLoader;
+import org.openide.util.Lookup;
+import org.openide.util.NbBundle.Messages;
+import org.openide.windows.TopComponent;
+
+@Messages({
+    "LBL_Rust_LOADER=Files of Rust"
+})
+@MIMEResolver.ExtensionRegistration(
+        displayName = "#LBL_Rust_LOADER",
+        mimeType = "test/x-rust-source",
+        extension = {".rs"})
+@DataObject.Registration(
+        mimeType = "test/x-rust-source",
+        iconBase = "com/github/drrb/rust/netbeans/rust_icon_small.png",
+        displayName = "#LBL_Rust_LOADER",
+        position = 300)
+@ActionReferences({
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "System", id = "org.openide.actions.OpenAction"),
+            position = 100,
+            separatorAfter = 200),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "Edit", id = "org.openide.actions.CutAction"),
+            position = 300),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "Edit", id = "org.openide.actions.CopyAction"),
+            position = 400,
+            separatorAfter = 500),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "Edit", id = "org.openide.actions.DeleteAction"),
+            position = 600),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "System", id = "org.openide.actions.RenameAction"),
+            position = 700,
+            separatorAfter = 800),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "System", id = "org.openide.actions.SaveAsTemplateAction"),
+            position = 900,
+            separatorAfter = 1000),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "System", id = "org.openide.actions.FileSystemAction"),
+            position = 1100,
+            separatorAfter = 1200),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "System", id = "org.openide.actions.ToolsAction"),
+            position = 1300),
+    @ActionReference(
+            path = "Loaders/test/x-rust-source/Actions",
+            id =
+            @ActionID(category = "System", id = "org.openide.actions.PropertiesAction"),
+            position = 1400)
+})
+public class RustDataObject extends MultiDataObject {
+
+    public RustDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
+        super(pf, loader);
+        registerEditor("test/x-rust-source", false);
+    }
+
+    @Override
+    protected int associateLookup() {
+        return 1;
+    }
+}
---
+++ src/main/java/com/github/drrb/rust/netbeans/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", content = "RustTemplate.rs")
+package com.github.drrb.rust.netbeans;
+
+import org.netbeans.api.templates.TemplateRegistration;
