14d5f93ac9bd242ca04a683df56d38da6585a8cd
drrb <drrrrrrrrrrrb@gmail.com>
2013-07-09 17:54:06 +1000

    Cleaned up generated file type classes

--- src/main/java/com/github/drrb/rust/netbeans/package-info.java
+++ src/main/java/com/github/drrb/rust/netbeans/package-info.java
@@ -2,7 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-@TemplateRegistration(folder = "Other", content = "RustTemplate.rs")
+
+@NbBundle.Messages({
+    "LBL_Rust_EMPTY_FILE_TEMPLATE=Empty Rust file"
+})
+@TemplateRegistration(folder = "Other", content = "RustTemplate.rs", displayName = "#LBL_Rust_EMPTY_FILE_TEMPLATE")
 package com.github.drrb.rust.netbeans;
 
 import org.netbeans.api.templates.TemplateRegistration;
+import org.openide.util.NbBundle;
