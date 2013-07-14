ae42304be1438eae95333daf4ba65e9c9d178ad1
drrb <drrrrrrrrrrrb@gmail.com>
2013-07-14 17:24:12 +1000

    Fixed "Fonts and Colors" dialog for Rust

--- src/main/resources/com/github/drrb/rust/netbeans/RustPreviewExample.rs
+++ src/main/resources/com/github/drrb/rust/netbeans/RustPreviewExample.rs
@@ -1,4 +1,9 @@
-fn main() {
-    let audience = "World";
-    io::println(fmt!("Hello, %?", audience));
+/**
+ * Say hello
+ */
+fn greet(recipient: &str) -> ~str {
+    // Print out the greetings
+    for 2.times |number| {
+        io::println(fmt!("%n: Hello, %s", number, recipient));
+    }
 }
