3ba3018a8f019ae9f2d9e335993805c472733cf0
drrb <drrrrrrrrrrrb@gmail.com>
2013-07-21 13:35:29 +1000

    Added skeletal code completion implementation

---
+++ src/main/java/com/github/drrb/rust/netbeans/RustCompletionItem.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2013 drrb
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.drrb.rust.netbeans;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JToolTip;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.StyledDocument;
+import org.netbeans.api.editor.completion.Completion;
+import org.netbeans.spi.editor.completion.CompletionItem;
+import org.netbeans.spi.editor.completion.CompletionResultSet;
+import org.netbeans.spi.editor.completion.CompletionTask;
+import org.netbeans.spi.editor.completion.support.AsyncCompletionQuery;
+import org.netbeans.spi.editor.completion.support.AsyncCompletionTask;
+import org.netbeans.spi.editor.completion.support.CompletionUtilities;
+import org.openide.util.Exceptions;
+import org.openide.util.ImageUtilities;
+
+/**
+ *
+ * @author drrb
+ */
+public class RustCompletionItem implements CompletionItem {
+
+    private static final ImageIcon fieldIcon = new ImageIcon(ImageUtilities.loadImage("com/github/drrb/rust/netbeans/rust_icon_small.png"));
+    private static final Color fieldColor = Color.decode("0x0000B2");
+    private final String text;
+    private final int dotOffset;
+    private final int caretOffset;
+
+    public RustCompletionItem(String text, int dotOffset, int caretOffset) {
+        this.text = text;
+        this.dotOffset = dotOffset;
+        this.caretOffset = caretOffset;
+    }
+
+    @Override
+    public void defaultAction(JTextComponent textComponent) {
+        try {
+            StyledDocument doc = (StyledDocument) textComponent.getDocument();
+            //Here we remove the characters starting at the start offset
+            //and ending at the point where the caret is currently found:
+            doc.remove(dotOffset, caretOffset - dotOffset);
+            doc.insertString(dotOffset, text, null);
+            Completion.get().hideAll();
+        } catch (BadLocationException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+
+    @Override
+    public void processKeyEvent(KeyEvent evt) {
+    }
+
+    @Override
+    public int getPreferredWidth(Graphics graphics, Font font) {
+        return CompletionUtilities.getPreferredWidth(text, null, graphics, font);
+    }
+
+    @Override
+    public void render(Graphics g, Font defaultFont, Color defaultColor, Color backgroundColor, int width, int height, boolean selected) {
+        CompletionUtilities.renderHtml(fieldIcon, text, null, g, defaultFont, (selected ? Color.white : fieldColor), width, height, selected);
+    }
+
+    @Override
+    public CompletionTask createDocumentationTask() {
+        return new AsyncCompletionTask(new AsyncCompletionQuery() {
+            @Override
+            protected void query(CompletionResultSet completionResultSet, Document document, int i) {
+                completionResultSet.setDocumentation(new RustCompletionDocumentation(RustCompletionItem.this));
+                completionResultSet.finish();
+            }
+        });
+    }
+
+    @Override
+    public CompletionTask createToolTipTask() {
+        return new AsyncCompletionTask(new AsyncCompletionQuery() {
+            @Override
+            protected void query(CompletionResultSet completionResultSet, Document document, int i) {
+                JToolTip toolTip = new JToolTip();
+                toolTip.setTipText(String.format("Press Enter to insert \"%s\"", text));
+                completionResultSet.setToolTip(toolTip);
+                completionResultSet.finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean instantSubstitution(JTextComponent component) {
+        return false;
+    }
+
+    @Override
+    public int getSortPriority() {
+        return 0;
+    }
+
+    @Override
+    public CharSequence getSortText() {
+        return text;
+    }
+
+    @Override
+    public CharSequence getInsertPrefix() {
+        return text;
+    }
+    
+    public String getText() {
+        return text;
+    }
+}
---
+++ src/main/java/com/github/drrb/rust/netbeans/RustCompletionProvider.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2013 drrb
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.drrb.rust.netbeans;
+
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.StyledDocument;
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.spi.editor.completion.CompletionProvider;
+import org.netbeans.spi.editor.completion.CompletionResultSet;
+import org.netbeans.spi.editor.completion.CompletionTask;
+import org.netbeans.spi.editor.completion.support.AsyncCompletionQuery;
+import org.netbeans.spi.editor.completion.support.AsyncCompletionTask;
+import org.openide.util.Exceptions;
+
+@MimeRegistration(mimeType = RustLanguage.MIME_TYPE, service = CompletionProvider.class)
+public class RustCompletionProvider implements CompletionProvider {
+
+    public RustCompletionProvider() {
+    }
+
+    @Override
+    public CompletionTask createTask(int queryType, JTextComponent textComponent) {
+
+        if (queryType == CompletionProvider.COMPLETION_QUERY_TYPE) {
+            return new AsyncCompletionTask(new RustCompletionQuery(), textComponent);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int getAutoQueryTypes(JTextComponent component, String typedText) {
+        return 0; //Only offer completion when it's explicitly requested
+    }
+
+    private static class RustCompletionQuery extends AsyncCompletionQuery {
+
+        @Override
+        protected void query(CompletionResultSet completionResultSet, Document document, int caretOffset) {
+            List<String> rustTokens = Arrays.asList("fn", "pub");
+
+            String filter = null;
+            int startOffset = caretOffset - 1;
+
+            try {
+                final StyledDocument bDoc = (StyledDocument) document;
+                final int lineStartOffset = getRowFirstNonWhite(bDoc, caretOffset);
+                final char[] line = bDoc.getText(lineStartOffset, caretOffset - lineStartOffset).toCharArray();
+                final int whiteOffset = indexOfWhite(line);
+                filter = new String(line, whiteOffset + 1, line.length - whiteOffset - 1);
+                if (whiteOffset > 0) {
+                    startOffset = lineStartOffset + whiteOffset + 1;
+                } else {
+                    startOffset = lineStartOffset;
+                }
+            } catch (BadLocationException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+
+            for (String rustToken : rustTokens) {
+                if (rustToken.startsWith(filter)) {
+                    completionResultSet.addItem(new RustCompletionItem(rustToken, startOffset, caretOffset));
+                }
+            }
+            completionResultSet.finish();
+        }
+    }
+
+    static int getRowFirstNonWhite(StyledDocument doc, int offset) throws BadLocationException {
+        Element lineElement = doc.getParagraphElement(offset);
+        int start = lineElement.getStartOffset();
+        while (start + 1 < lineElement.getEndOffset()) {
+            try {
+                if (doc.getText(start, 1).charAt(0) != ' ') {
+                    break;
+                }
+            } catch (BadLocationException ex) {
+                throw (BadLocationException) new BadLocationException(
+                        "calling getText(" + start + ", " + (start + 1)
+                        + ") on doc of length: " + doc.getLength(), start).initCause(ex);
+            }
+            start++;
+        }
+        return start;
+    }
+
+    static int indexOfWhite(char[] line) {
+        int i = line.length;
+        while (--i > -1) {
+            final char c = line[i];
+            if (Character.isWhitespace(c)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
