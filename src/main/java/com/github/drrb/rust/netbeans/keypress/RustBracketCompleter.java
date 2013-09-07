0fb0a400cde7eeedfda7c52a6e1acdd3985e1193
drrb <drrrrrrrrrrrb@gmail.com>
2013-09-08 01:23:13 +1000

    Added parentheses completion

---
+++ src/main/java/com/github/drrb/rust/netbeans/keypress/RustBracketCompleter.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2013 drrb
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.drrb.rust.netbeans.keypress;
+
+import com.github.drrb.rust.netbeans.RustLanguage;
+import javax.swing.text.BadLocationException;
+import org.netbeans.api.editor.mimelookup.MimePath;
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.spi.editor.typinghooks.TypedTextInterceptor;
+
+/**
+ *
+ */
+public class RustBracketCompleter implements TypedTextInterceptor {
+
+    @Override
+    public boolean beforeInsert(Context context) throws BadLocationException {
+        return false;
+    }
+
+    @Override
+    public void insert(MutableContext context) throws BadLocationException {
+        if (context.getText().equals("(")) {
+            context.setText("()", 1);
+        }
+    }
+
+    @Override
+    public void afterInsert(Context context) throws BadLocationException {
+    }
+
+    @Override
+    public void cancelled(Context context) {
+    }
+
+    @MimeRegistration(mimeType = RustLanguage.MIME_TYPE, service = TypedTextInterceptor.Factory.class)
+    public static class Factory implements TypedTextInterceptor.Factory {
+
+        @Override
+        public TypedTextInterceptor createTypedTextInterceptor(MimePath mimePath) {
+            if (RustLanguage.MIME_TYPE.equals(mimePath.getPath())) {
+                return new RustBracketCompleter();
+            } else {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ src/test/java/com/github/drrb/rust/netbeans/keypress/RustBracketCompleterTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2013 drrb
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.drrb.rust.netbeans.keypress;
+
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import org.netbeans.spi.editor.typinghooks.TypedTextInterceptor.Context;
+import org.netbeans.spi.editor.typinghooks.TypedTextInterceptor.MutableContext;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ *
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(MutableContext.class)
+public class RustBracketCompleterTest {
+
+    private RustBracketCompleter interceptor;
+
+    @Before
+    public void setUp() {
+        interceptor = new RustBracketCompleter();
+    }
+
+    @Test
+    public void shouldNotStopAnEdit() throws Exception {
+        Context context = mock(Context.class);
+        assertThat(interceptor.beforeInsert(context), is(false));
+    }
+
+    @Test
+    public void shouldAddClosingBracketIfTypingAnOpeningBracket() throws Exception {
+        MutableContext context = mock(MutableContext.class);
+        when(context.getText()).thenReturn("(");
+        interceptor.insert(context);
+        verify(context).setText("()", 1);
+    }
+
+    @Test
+    public void shouldIgnoreTypingSomethingElse() throws Exception {
+        MutableContext context = mock(MutableContext.class);
+        when(context.getText()).thenReturn("x");
+        verify(context, never()).setText(anyString(), anyInt());
+        interceptor.insert(context);
+    }
+
+}
