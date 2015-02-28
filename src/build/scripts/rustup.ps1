2ce5c7a39157fbbbea1297e9040e1c5954623d10
drrb <drrrrrrrrrrrb@gmail.com>
2015-02-28 21:02:42 +1100

    Trying to figure out powershell for installer

--- src/build/scripts/rustup.ps1
+++ src/build/scripts/rustup.ps1
@@ -9,13 +9,14 @@ if ([environment]::Is64BitOperatingSystem) {
 }
 
 $package = "rust-nightly-$($arch)-pc-windows-gnu.exe"
+$downloaded_package = ".\$($package)"
 $url = "https://static.rust-lang.org/dist/$($package)"
 
 echo "Downloading Rust and Cargo from $($url)"
 Start-FileDownload $url
 
 echo "Installing Rust into $($install_dir)"
-& ".\$($package)" /VERYSILENT /NORESTART /DIR="$($install_dir)"
+$downloaded_package /VERYSILENT /NORESTART /DIR="$($install_dir)"
 
 echo "Adding $($install_dir)\bin to the path"
 [Environment]::SetEnvironmentVariable("Path", [System.Environment]::GetEnvironmentVariable("Path", "User") + "$($install_dir)\bin", "User")
