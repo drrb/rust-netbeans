1f15659b4e010ccc9783d3593325f920da24a809
drrb <drrrrrrrrrrrb@gmail.com>
2015-02-28 18:32:55 +1100

    Added debugging to installer script

--- src/build/scripts/rustup.ps1
+++ src/build/scripts/rustup.ps1
@@ -14,4 +14,13 @@ echo "Installing Rust into $($install_dir)"
 echo "Adding $($install_dir)\bin to the path"
 [Environment]::SetEnvironmentVariable("Path", [System.Environment]::GetEnvironmentVariable("Path", "User") + "$($install_dir)\bin", "User")
 
+echo "C:\"
+ls "C:\"
+
+echo "C:\Program Files"
+ls "C:\Program Files"
+
+echo "C:\Program Files (x86)"
+ls "C:\Program Files (x86)"
+
 echo "Rust and Cargo are ready to roll!"
