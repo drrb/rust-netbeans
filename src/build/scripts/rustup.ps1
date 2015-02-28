9680a8f31cf34a3465743be14b8e99bd3ead43aa
drrb <drrrrrrrrrrrb@gmail.com>
2015-02-28 18:30:04 +1100

    Adjusting path again

--- src/build/scripts/rustup.ps1
+++ src/build/scripts/rustup.ps1
@@ -12,6 +12,6 @@ echo "Installing Rust into $($install_dir)"
 & ".\$($package)" /VERYSILENT /NORESTART /DIR=C:\Rust
 
 echo "Adding $($install_dir)\bin to the path"
-[Environment]::SetEnvironmentVariable("Path", [System.Environment]::GetEnvironmentVariable("Path", "User") + ";$($install_dir)\bin", "User")
+[Environment]::SetEnvironmentVariable("Path", [System.Environment]::GetEnvironmentVariable("Path", "User") + "$($install_dir)\bin", "User")
 
 echo "Rust and Cargo are ready to roll!"
